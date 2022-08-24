2c8c97c5f286ece7c5291db4afd8a6f4a98acbbc
sooraj671 <75793312+sooraj671@users.noreply.github.com>
2022-08-24 13:57:33 +0500

    react-practice-fitness-clone
    Signed-off-by: sooraj671 <75793312+sooraj671@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Hero from './components/Hero/Hero';
+import Header from './components/Header/Header'
+import Program from './components/Programs/Program';
+import Reason from './components/Reasons/Reason';
+import Plan from './components/Plans/Plan'
+import Testimonials from './components/Testimonials/Testimonials';
+import Join from './components/Join/Join';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+          <Hero/>
+          <Program/>
+          <Reason/>
+          <Plan/>
+          <Testimonials/>
+          <Join/>
+          <Footer/>
     </div>
   );
 }
